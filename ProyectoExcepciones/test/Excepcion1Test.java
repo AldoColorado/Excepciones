50b55a3b290b90df176a5e397e6f3967912581ec
rendo <rendo@DESKTOP-0FGPCGK>
2020-05-13 21:20:04 -0500

    TestExcep1

---
+++ ProyectoExcepciones/test/Excepcion1Test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Excepcion1Test {
+    
+    public Excepcion1Test() {
+    }
+    
+}
